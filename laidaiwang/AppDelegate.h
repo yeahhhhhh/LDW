6d606e3a9ed283e261a6e44293977827e72e102e
yeahhhhhh <904603513@qq.com>
2016-06-13 10:50:19 +0800

    Initial Commit

---
+++ laidaiwang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  laidaiwang
+//
+//  Created by 黄欣 on 16/6/13.
+//  Copyright © 2016年 黄欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ laidaiwang/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  laidaiwang
+//
+//  Created by 黄欣 on 16/6/13.
+//  Copyright © 2016年 黄欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
